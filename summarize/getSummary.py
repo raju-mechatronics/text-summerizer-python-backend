78bec278802c912be381bd1ca562d02da66718da
samiulalimsaad <samiulalimsaad@gmail.com>
2023-03-14 01:19:36 +0600

    updated

--- summarize/getSummary.py
+++ summarize/getSummary.py
@@ -4,7 +4,6 @@ from __future__ import absolute_import
 from __future__ import division, print_function, unicode_literals
 
 from sumy.parsers.html import HtmlParser
-from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
